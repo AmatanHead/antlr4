66bb177b7327decdc92b40cae26da381249de627
Vladimir Goncharov <amatanhead@yandex-team.ru>
2019-01-16 00:05:54 +0300

    Add memory pool

---
+++ runtime/C/antlr4/memory/pool.c
@@ -0,0 +1,206 @@
+#include "pool.h"
+
+#include <assert.h>
+#include <memory.h>
+#include <stdalign.h>
+#include <stddef.h>
+#include <errno.h>
+#include <stdio.h>
+
+
+static inline size_t A4_AlignDown(size_t len, size_t align) {
+    assert(align > 0 && (align & (align - 1)) == 0);  // is power of 2
+    return len & ~(align - 1);
+}
+
+static inline size_t A4_AlignUp(size_t len, size_t align) {
+    const size_t alignedResult = A4_AlignDown(len + (align - 1), align);
+    assert(alignedResult >= len);  // check for overflow
+    return alignedResult;
+}
+
+static inline char* A4_AlignUpMem(char* mem, size_t align) {
+    return (char*)A4_AlignUp((size_t)mem, align);
+}
+
+
+// Pool page implementation
+// --------------------------------------------------------------------------------------------------------------------
+
+struct A4_MemoryPoolPage {
+    /// Non-owning pointer to the first unused byte in the memory chunk.
+    char*                     current;
+    /// How many bytes left in the chunk.
+    size_t                    left;
+    /// Owning pointer to the next page (used up previously).
+    struct A4_MemoryPoolPage* next;
+};
+
+/// Take ownership over an old page and allocate a new one. If this function
+/// returns NULL, new page is not created and ownership over an old page is not taken.
+static inline struct A4_MemoryPoolPage* A4_MemoryPoolPage_New(struct A4_MemoryPoolPage* old, size_t size) {
+    struct A4_MemoryPoolPage* page = malloc(sizeof(struct A4_MemoryPoolPage) + size);
+    if (!page) return NULL;
+
+    page->current = (char*)(page + 1);
+    page->left = size;
+    page->next = old;
+
+    return page;
+}
+
+/// Free chain of pages.
+static inline void A4_MemoryPoolPage_Delete(struct A4_MemoryPoolPage* page) {
+    while (page) {
+        struct A4_MemoryPoolPage* next = page->next;
+        free(page);
+        page = next;
+    }
+}
+
+/// Get total size of the page.
+static inline size_t A4_MemoryPoolPage_Size(const struct A4_MemoryPoolPage* page) {
+    if (!page)
+        return 0;
+    else
+        return page->current + page->left - (const char*)(page);
+}
+
+/// Get number of bytes left on this page.
+static inline size_t A4_MemoryPoolPage_Left(const struct A4_MemoryPoolPage* page) {
+    if (!page)
+        return 0;
+    else
+        return page->left;
+}
+
+/// Try allocate new memory on the page.
+static inline void* A4_MemoryPoolPage_Alloc(struct A4_MemoryPoolPage* page, size_t size) {
+    if (!page) return NULL;
+    if (page->left < size) return NULL;
+
+    char* ret = page->current;
+
+    page->current += size;
+    page->left -= size;
+
+    return ret;
+}
+
+/// Try allocate new memory on the page.
+static inline void* A4_MemoryPoolPage_AllocAligned(struct A4_MemoryPoolPage* page, size_t size, size_t align) {
+    if (!page) return NULL;
+
+    size_t pad = A4_AlignUpMem(page->current, align) - page->current;
+
+    void* ret = A4_MemoryPoolPage_Alloc(page, size + pad);
+
+    if (ret) {
+        return (char*)(ret) + pad;
+    } else {
+        return NULL;
+    }
+}
+
+
+// Pool implementation
+// --------------------------------------------------------------------------------------------------------------------
+
+struct A4_MemoryPool {
+    /// Current options.
+    struct A4_MemoryPool_Options options;
+    /// Current page.
+    struct A4_MemoryPoolPage*    page;
+    /// Size of all pages except for the current one.
+    size_t                       allocated;
+    /// Allocated but unused memory on all pages except for the current one.
+    size_t                       waste;
+    /// Size of the next block.
+    size_t                       next_page_size;
+};
+
+struct A4_MemoryPool* A4_MemoryPool_New(struct A4_MemoryPool_Options options) {
+    struct A4_MemoryPool* pool = malloc(sizeof(struct A4_MemoryPool));
+    if (!pool) return NULL;
+
+    pool->page = NULL;
+    pool->options = options;
+    pool->allocated = 0;
+    pool->waste = 0;
+    pool->next_page_size = options.page_size;
+
+    return pool;
+}
+
+void A4_MemoryPool_Delete(struct A4_MemoryPool* pool) {
+    if (pool) A4_MemoryPoolPage_Delete(pool->page);
+    free(pool);
+}
+
+void A4_MemoryPool_Clear(struct A4_MemoryPool* pool) {
+    assert(pool);
+    A4_MemoryPoolPage_Delete(pool->page);
+    pool->page = NULL;
+    pool->allocated = 0;
+    pool->waste = 0;
+    pool->next_page_size = pool->options.page_size;
+}
+
+static A4_Errno A4_MemoryPool_AddPageFor(struct A4_MemoryPool* pool, size_t size) {
+    assert(pool);
+
+    size_t page_size = pool->next_page_size;
+    if (page_size < size)
+        page_size = size;
+
+    struct A4_MemoryPoolPage* page = A4_MemoryPoolPage_New(pool->page, page_size);
+    if (!page) return ENOMEM;
+
+    pool->allocated += A4_MemoryPoolPage_Size(pool->page);
+    pool->waste += A4_MemoryPoolPage_Left(pool->page);
+    pool->page = page;
+
+    switch (pool->options.growth_strategy) {
+        case A4_GS_LINEAR:
+            pool->next_page_size = page_size;
+            break;
+        case A4_GS_EXPONENT:
+            pool->next_page_size = page_size * 2;
+            break;
+    }
+
+    return A4_SUCCESS;
+}
+
+size_t A4_MemoryPool_Allocated(const struct A4_MemoryPool* pool) {
+    assert(pool);
+    return pool->allocated + A4_MemoryPoolPage_Size(pool->page);
+}
+
+size_t A4_MemoryPool_Left(const struct A4_MemoryPool* pool) {
+    assert(pool);
+    return A4_MemoryPoolPage_Left(pool->page);
+}
+
+size_t A4_MemoryPool_Waste(const struct A4_MemoryPool* pool) {
+    assert(pool);
+    return pool->waste + A4_MemoryPoolPage_Left(pool->page);
+}
+
+void* A4_MemoryPool_Malloc(struct A4_MemoryPool* pool, size_t size) {
+    assert(pool);
+    // size = A4_AlignUp(size, alignof(max_align_t));
+    void* ret = A4_MemoryPoolPage_Alloc(pool->page, size);
+    if (ret) return ret;
+    A4_MemoryPool_AddPageFor(pool, size);
+    return A4_MemoryPoolPage_Alloc(pool->page, size);
+}
+
+void* A4_MemoryPool_MallocAligned(struct A4_MemoryPool* pool, size_t size, size_t align) {
+    assert(pool);
+    // size = A4_AlignUp(size, alignof(max_align_t));
+    void* ret = A4_MemoryPoolPage_AllocAligned(pool->page, size, align);
+    if (ret) return ret;
+    A4_MemoryPool_AddPageFor(pool, size + align - 1);
+    return A4_MemoryPoolPage_AllocAligned(pool->page, size, align);
+}
---
+++ runtime/C/antlr4/memory/pool.h
@@ -0,0 +1,75 @@
+#pragma once
+
+#include "../antlr4.h"
+
+#include <stdbool.h>
+#include <stdlib.h>
+
+HEADER_BEGIN
+
+/**
+ * Note: this pool is not thread safe.
+ */
+struct A4_MemoryPool;
+
+
+// Memory pool options
+// --------------------------------------------------------------------------------------------------------------------
+
+/// Options for memory pool growth rate.
+enum A4_MemoryPool_GrowthStrategy {
+    /// Each new page is as big as the previous one.
+    A4_GS_LINEAR,
+    /// Each new page twice as big as the previous one.
+    A4_GS_EXPONENT,
+};
+
+/// Options for memory pool init.
+struct A4_MemoryPool_Options {
+    /// Size of the first page.
+    size_t page_size;
+    /// How page size changes over time.
+    enum A4_MemoryPool_GrowthStrategy growth_strategy;
+};
+
+/// Construct default options for memory pool init.
+inline struct A4_MemoryPool_Options A4_MemoryPool_Options_Default() {
+    return (struct A4_MemoryPool_Options) {
+        .page_size = 2048,
+        .growth_strategy = A4_GS_EXPONENT
+    };
+}
+
+
+// Memory pool API
+// --------------------------------------------------------------------------------------------------------------------
+
+/// Allocate and init new memory pool.
+struct A4_MemoryPool* A4_MemoryPool_New(struct A4_MemoryPool_Options options);
+inline struct A4_MemoryPool* A4_MemoryPool_NewDefault() { return A4_MemoryPool_New(A4_MemoryPool_Options_Default()); }
+
+/// Delete memory pool and free all its memory.
+void A4_MemoryPool_Delete(struct A4_MemoryPool* pool);
+
+/// Release all memory stored in the pool.
+void A4_MemoryPool_Clear(struct A4_MemoryPool* pool);
+
+/// Get total number of bytes that's currently allocated.
+size_t A4_MemoryPool_Allocated(const struct A4_MemoryPool* pool);
+
+/// Get number of bytes left on the current page.
+size_t A4_MemoryPool_Left(const struct A4_MemoryPool* pool);
+
+/// Get number of bytes that are allocated but unused.
+size_t A4_MemoryPool_Waste(const struct A4_MemoryPool* pool);
+
+/// Allocate a new chunk of unaligned memory on the pool.
+void* A4_MemoryPool_Malloc(struct A4_MemoryPool* pool, size_t size);
+
+/// Allocate a new chunk of memory on the pool with the given alignment.
+void* A4_MemoryPool_MallocAligned(struct A4_MemoryPool* pool, size_t size, size_t align);
+
+/// Allocate object of the given type.
+#define A4_POOL_ALLOC(pool, T) (T*)A4_MemoryPool_MallocAligned((pool), sizeof(T), alignof(T));
+
+HEADER_END
